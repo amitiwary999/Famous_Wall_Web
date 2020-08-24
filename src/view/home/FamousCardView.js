d77927304d25b14909e8c5f88a88c21ae17a7566
amitiwary999 <tiwarya9@gmail.com>
2020-08-24 23:13:35 +0530

    working on item for famous card view

--- src/view/home/FamousCardView.js
+++ src/view/home/FamousCardView.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import imgPic from '../../img/IMG_20190907_224201.jpg'
 
 const FamousCardView = () => {
-    return(
+    return (
+      <div className="container">
+        <div className="md-4">
         <div className="card">
+            <div className="row">
 
+            </div>
+            <div>
+            <img src={imgPic} style={{width: 'auto', height: 'auto', maxHeight: '640px', minWidth: '120px'}} />
+            </div>
+            
+            <div className="row">
+                
+            </div>
         </div>
-    )
+        </div>
+      </div>
+    );
 }
 
 export default FamousCardView;
