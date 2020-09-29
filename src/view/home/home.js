75c5f7c9f7695878e680cce01990139fe656e3ec
amitiwary999 <tiwarya9@gmail.com>
2020-09-29 23:39:54 +0530

    updated design to show video call request

--- src/view/home/home.js
+++ src/view/home/home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react'
 import InfiniteScroll from 'react-infinite-scroller'
-import { Card, Badge, Col, CardBody, Button } from 'reactstrap'
+import { Card, Badge, Col, CardBody, Button, CardHeader } from 'reactstrap'
 import { Video, Image, Music } from 'react-feather'
 import Dropzone from 'react-dropzone'
 import FamousCardView from './FamousCardView'
@@ -77,83 +77,100 @@ const Home = () => {
         <Col md={4} sm={6} className="mx-auto mt-2">
           <Card>
             <CardBody>
+              <div className="d-flex justify-content-end mb-2">
+                <Badge color="success" className=" mr-1 mb-1">
+                  <Badge pill color="danger" className="p-1 badge-up float-right" style={{marginRight: -8, marginTop: -6, fontSize: '12px'}}>
+                    5
+                  </Badge>
+                  <Col>
+                    <Video size={16} />
+                    <p className="font-weight-bold">Accept request</p>
+                  </Col>
+                </Badge>
+              </div>
               <p className="font-weight-bold">Make yourself famous</p>
-              {currentUser && <div className="d-flex justify-content-between pl-2 pr-2">
-                <Dropzone
-                  accept="video/*"
-                  onDrop={(acceptedFiles) => {
-                    console.log(acceptedFiles);
-                    setSelectedMediaFile(acceptedFiles[0]);
-                    setSelectedMediaType(VIDEO_MEDIA);
-                    setShowSelectedMediaCard(true);
-                  }}
-                >
-                  {({ getRootProps, getInputProps }) => (
-                    <section>
-                      <div
-                        style={{ cursor: "pointer" }}
-                        {...getRootProps({
-                          onClick: (event) => dropzoneClick(event),
-                        })}
-                      >
-                        <input {...getInputProps()} />
-                        <Badge pill color="info" className="mr-1 mb-1">
-                          <Video size={16} className="mr-2" />
-                          Video
-                        </Badge>
-                      </div>
-                    </section>
-                  )}
-                </Dropzone>
-                <Dropzone
-                  accept="image/*"
-                  onDrop={(acceptedFiles) => {
-                    console.log(acceptedFiles);
-                    setSelectedMediaFile(acceptedFiles[0]);
-                    setSelectedMediaType(IMAGE_MEDIA);
-                    setShowSelectedMediaCard(true);
-                  }}
-                >
-                  {({ getRootProps, getInputProps }) => (
-                    <section>
-                      <div
-                        style={{ cursor: "pointer" }}
-                        {...getRootProps({
-                          onClick: (event) => dropzoneClick(event),
-                        })}
-                      >
-                        <input {...getInputProps()} />
-                        <Badge pill color="info" className="mr-1 mb-1">
-                          <Image size={16} className="mr-2" />
-                          Image
-                        </Badge>
-                      </div>
-                    </section>
-                  )}
-                </Dropzone>
-                <Dropzone
-                  accept="audio/*"
-                  onDrop={(acceptedFiles) => console.log(acceptedFiles)}
-                >
-                  {({ getRootProps, getInputProps }) => (
-                    <section>
-                      <div
-                        style={{ cursor: "pointer" }}
-                        {...getRootProps({
-                          onClick: (event) => dropzoneClick(event),
-                        })}
-                      >
-                        <input {...getInputProps()} />
-                        <Badge pill color="info" className="mr-1 mb-1">
-                          <Music size={16} className="mr-2" />
-                          Audio
-                        </Badge>
-                      </div>
-                    </section>
-                  )}
-                </Dropzone>
-              </div>}
-              {!currentUser && <Button className="primary" onClick={() => setShowLogin(true)}>Please Login </Button>}
+              {currentUser && (
+                <div className="d-flex justify-content-between pl-2 pr-2">
+                  <Dropzone
+                    accept="video/*"
+                    onDrop={(acceptedFiles) => {
+                      console.log(acceptedFiles);
+                      setSelectedMediaFile(acceptedFiles[0]);
+                      setSelectedMediaType(VIDEO_MEDIA);
+                      setShowSelectedMediaCard(true);
+                    }}
+                  >
+                    {({ getRootProps, getInputProps }) => (
+                      <section>
+                        <div
+                          style={{ cursor: "pointer" }}
+                          {...getRootProps({
+                            onClick: (event) => dropzoneClick(event),
+                          })}
+                        >
+                          <input {...getInputProps()} />
+                          <Badge pill color="info" className="mr-1 mb-1">
+                            <Video size={16} className="mr-2" />
+                            Video
+                          </Badge>
+                        </div>
+                      </section>
+                    )}
+                  </Dropzone>
+                  <Dropzone
+                    accept="image/*"
+                    onDrop={(acceptedFiles) => {
+                      console.log(acceptedFiles);
+                      setSelectedMediaFile(acceptedFiles[0]);
+                      setSelectedMediaType(IMAGE_MEDIA);
+                      setShowSelectedMediaCard(true);
+                    }}
+                  >
+                    {({ getRootProps, getInputProps }) => (
+                      <section>
+                        <div
+                          style={{ cursor: "pointer" }}
+                          {...getRootProps({
+                            onClick: (event) => dropzoneClick(event),
+                          })}
+                        >
+                          <input {...getInputProps()} />
+                          <Badge pill color="info" className="mr-1 mb-1">
+                            <Image size={16} className="mr-2" />
+                            Image
+                          </Badge>
+                        </div>
+                      </section>
+                    )}
+                  </Dropzone>
+                  <Dropzone
+                    accept="audio/*"
+                    onDrop={(acceptedFiles) => console.log(acceptedFiles)}
+                  >
+                    {({ getRootProps, getInputProps }) => (
+                      <section>
+                        <div
+                          style={{ cursor: "pointer" }}
+                          {...getRootProps({
+                            onClick: (event) => dropzoneClick(event),
+                          })}
+                        >
+                          <input {...getInputProps()} />
+                          <Badge pill color="info" className="mr-1 mb-1">
+                            <Music size={16} className="mr-2" />
+                            Audio
+                          </Badge>
+                        </div>
+                      </section>
+                    )}
+                  </Dropzone>
+                </div>
+              )}
+              {!currentUser && (
+                <Button className="primary" onClick={() => setShowLogin(true)}>
+                  Please Login{" "}
+                </Button>
+              )}
             </CardBody>
           </Card>
         </Col>
