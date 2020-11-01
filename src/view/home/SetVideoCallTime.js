7a1248da9aa9289c8343b98c89af0fe555ef282e
amitiwary999 <tiwarya9@gmail.com>
2020-11-01 22:54:07 +0530

    added eslint auto fix, and default date and time in accept videor equest

--- src/view/home/SetVideoCallTime.js
+++ src/view/home/SetVideoCallTime.js
@@ -1,82 +1,91 @@
+/* eslint-disable no-empty */
+/* eslint-disable consistent-return */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-filename-extension */
 import React, { useState } from 'react';
-import { Button, Col, Label, Modal, ModalBody, ModalFooter, Row } from 'reactstrap';
-import Flatpickr from "react-flatpickr";
-import moment from 'moment'
+import {
+  Button, Col, Label, Modal, ModalBody, ModalFooter, Row,
+} from 'reactstrap';
+import Flatpickr from 'react-flatpickr';
+import moment from 'moment';
 import { notify } from '../../common/util';
 
-const SetVideoCallTime = props => {
-    const [callDate, setCallDate] = useState(null);
-    const [callTime, setCallTime] = useState(null);
-    let requestorId = props.requestorId;
-    const confirmTime = () => {
-        //selected date and time should be greater than current.
-        let selectedDate = moment(
-            callDate
-        ).format("YYYY-MM-DD");
-        let selectTime = moment(
-            new Date(callTime)
-        ).format("HH:mm:ss");
+const SetVideoCallTime = (props) => {
+  const [callDate, setCallDate] = useState(new Date());
+  const [callTime, setCallTime] = useState(new Date());
+  const { requestorId } = props;
+  const confirmTime = () => {
+    // selected date and time should be greater than current.
+    const selectedDate = moment(
+      callDate,
+    ).format('YYYY-MM-DD');
+    const selectTime = moment(
+      new Date(callTime),
+    ).format('HH:mm:ss');
 
-        if(!callDate || !callTime){
+    if (!callDate || !callTime) {
 
-        } else if (moment(selectedDate + " " + selectTime)
-            .utc().isBefore(moment().utc().valueOf())
-          ) {
-              notify("You can't set video call time in past date","danger","Error!");
-              return false;
-            } else {
-               let dateTime = moment(selectedDate + " " + selectTime).utc().format("YYYY-MM-DD HH:mm:ss");
-              props.confirmTime(requestorId, 1, dateTime);
-            }
+    } else if (moment(`${selectedDate} ${selectTime}`)
+      .utc().isBefore(moment().utc().valueOf())
+    ) {
+      notify("You can't set video call time in past date", 'danger', 'Error!');
+      return false;
+    } else {
+      const dateTime = moment(`${selectedDate} ${selectTime}`).utc().format('YYYY-MM-DD HH:mm:ss');
+      props.confirmTime(requestorId, 1, dateTime);
     }
+  };
 
-    const close = () => {
-        props.close();
-    }
+  const close = () => {
+    props.close();
+  };
 
-    return (
-      <Modal isOpen={true} toggle={close} className="modal-lg">
-        <ModalBody>
-          <Row>
-            <Col>
-              <Label for="selectdate" className="font-weight-bold">
-                Select Date
-              </Label>
-              <Flatpickr
-                id="startTime"
-                className="form-control bg-white"
-                value={callDate}
-                onChange={(date) => {
-                  setCallDate(date[0]);
-                }}
-              />
-            </Col>
+  return (
+    <Modal isOpen toggle={close} className="modal-lg">
+      <ModalBody>
+        <Row>
+          <Col>
+            <Label for="selectdate" className="font-weight-bold">
+              Select Date
+            </Label>
+            <Flatpickr
+              id="startTime"
+              className="form-control bg-white"
+              value={callDate}
+              options={{
+                minDate: 'today',
+              }}
+              onChange={(date) => {
+                setCallDate(date[0]);
+              }}
+            />
+          </Col>
 
-            <Col>
-              <Label for="selectTime" className="font-weight-bold">
-                Select Time
-              </Label>
-              <Flatpickr
-                id="startTime"
-                className="form-control bg-white"
-                options={{
-                  enableTime: true,
-                  noCalendar: true,
-                  dateFormat: "h:i K",
-                }}
-                value={callTime}
-                onChange={(date) => {
-                setCallTime(date)
-                }}
-              />
-            </Col>
-          </Row>
-        </ModalBody>
-        <ModalFooter>
-            <Button color="primary" onClick={confirmTime}>Confirm</Button>
-        </ModalFooter>
-      </Modal>
-    );
-}
+          <Col>
+            <Label for="selectTime" className="font-weight-bold">
+              Select Time
+            </Label>
+            <Flatpickr
+              id="startTime"
+              className="form-control bg-white"
+              options={{
+                enableTime: true,
+                noCalendar: true,
+                dateFormat: 'h:i K',
+              }}
+              value={callTime}
+              onChange={(date) => {
+                setCallTime(date);
+              }}
+            />
+          </Col>
+        </Row>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="primary" onClick={confirmTime}>Confirm</Button>
+      </ModalFooter>
+    </Modal>
+  );
+};
 
 export default SetVideoCallTime;
--- src/view/home/VideoRequestList/VideoRequestLists.js
+++ src/view/home/VideoRequestList/VideoRequestLists.js
@@ -1,71 +1,74 @@
-import React from 'react'
-import { Button, Card, CardBody, Col, Container } from 'reactstrap'
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/prop-types */
+import React from 'react';
+import {
+  Button, Card, CardBody, Col,
+} from 'reactstrap';
 import moment from 'moment';
-import './VideoRequestList.css'
+import './VideoRequestList.css';
 import { Delete } from 'react-feather';
 
-const VideoRequestLists = props => {
-    let videoRequests = props.videoRequests;
-    const videolist = (item, index) => {
-        let profileImg = item.userDp;
-        let name = item.userName;
-        return(
-            <Card key={index}>
-             
-                <CardBody className="p-0" style={{marginTop: '5px', marginBottom: '5px'}}>
-                   <Delete className="float-right mr-1" onClick={() => props.updateRequest(item.userId, 2)}/>
-                <div className="col" key={index}>
-                  <div className="row">
-                    <img
-                      src={profileImg}
-                      className="requestorImg"
-                      alt={name}
-                    />
+const VideoRequestLists = (props) => {
+  const { videoRequests } = props;
+  const videolist = (item, index) => {
+    const profileImg = item.userDp;
+    const name = item.userName;
+    return (
+      <Card key={index}>
+
+        <CardBody className="p-0" style={{ marginTop: '5px', marginBottom: '5px' }}>
+          <Delete className="float-right mr-1" onClick={() => props.updateRequest(item.userId, 2)} />
+          <div className="col" key={index}>
+            <div className="row">
+              <img
+                src={profileImg}
+                className="requestorImg"
+                alt={name}
+              />
+              <div className="row">
+                <div className="col">
+                  <div className="col">
+                    <p className="text-center font-weight-bold requestorName">{name}</p>
+                  </div>
+                  {item.status === 0 && (
                   <div className="row">
                     <div className="col">
-                      <div className="col">
-                        <p className="text-center font-weight-bold requestorName">{name}</p>
-                      </div>
-                      {item.status == 0 && (<div className="row">
-                        <div className="col">
-                          <Button
-                          className="acceptButton"
-                            color="primary"
-                            onClick={() => props.updateRequest(item.userId, 1)}
-                          >
-                            Accept
-                          </Button>
-                        </div>
-                      </div>)}
-                      {item.status == 1 && (
-                        <div className="row">
-                          <div className="col">
-                            <p>
-                              {moment(item.updatedAt).fromNow()}
-                            </p>
-                          </div>  
-                        </div>
-                      )}
+                      <Button
+                        className="acceptButton"
+                        color="primary"
+                        onClick={() => props.updateRequest(item.userId, 1)}
+                      >
+                        Accept
+                      </Button>
                     </div>
                   </div>
+                  )}
+                  {item.status === 1 && (
+                  <div className="row">
+                    <div className="col">
+                      <p>
+                        {moment(item.updatedAt).fromNow()}
+                      </p>
+                    </div>
                   </div>
+                  )}
                 </div>
-                </CardBody>
-            </Card>
-        )
-    }
-    return(
-        <Col>
-            <Card>
-                <CardBody>
-                    {videoRequests.map((item, index) => {
-                            return videolist(item, index)
-                     })
-                    }
-                </CardBody>
-            </Card>
-        </Col>
-    )
-}
+              </div>
+            </div>
+          </div>
+        </CardBody>
+      </Card>
+    );
+  };
+  return (
+    <Col>
+      <Card>
+        <CardBody>
+          {videoRequests.map((item, index) => videolist(item, index))}
+        </CardBody>
+      </Card>
+    </Col>
+  );
+};
 
 export default VideoRequestLists;
