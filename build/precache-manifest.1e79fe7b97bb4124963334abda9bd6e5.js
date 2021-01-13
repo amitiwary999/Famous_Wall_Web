d26c647d020ba2ba144dbea9714f066e4ad5fabb
amitiwary999 <tiwarya9@gmail.com>
2021-01-13 23:02:04 +0530

    added the readme

---
+++ build/precache-manifest.1e79fe7b97bb4124963334abda9bd6e5.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228a5d629dd9287c55f58b01c106232d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "334282b7ab30a48860a8",
+    "url": "/static/css/4.6b36cc0b.chunk.css"
+  },
+  {
+    "revision": "59e6c67e9348ac58c959",
+    "url": "/static/css/6.71fe6cbe.chunk.css"
+  },
+  {
+    "revision": "1342df4fcab80e110a2e",
+    "url": "/static/css/7.b69c7eb6.chunk.css"
+  },
+  {
+    "revision": "587378361b9b4b061e0e",
+    "url": "/static/css/main.81f37c3a.chunk.css"
+  },
+  {
+    "revision": "b438d620b0c7bdae4d62",
+    "url": "/static/js/0.37fb7162.chunk.js"
+  },
+  {
+    "revision": "2eb332978e3077966e3e",
+    "url": "/static/js/1.6af329bc.chunk.js"
+  },
+  {
+    "revision": "8e7fa176b006150306288bd092a696c0",
+    "url": "/static/js/1.6af329bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a96a4b472b3f893f257",
+    "url": "/static/js/10.65a30ae4.chunk.js"
+  },
+  {
+    "revision": "334282b7ab30a48860a8",
+    "url": "/static/js/4.33e0f3a3.chunk.js"
+  },
+  {
+    "revision": "22c95ed68071a711843b9981e2bca1af",
+    "url": "/static/js/4.33e0f3a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cac2010d8cd75e2f950",
+    "url": "/static/js/5.92d3cc63.chunk.js"
+  },
+  {
+    "revision": "d11469acfc6ba17a87637fdddceb86c4",
+    "url": "/static/js/5.92d3cc63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59e6c67e9348ac58c959",
+    "url": "/static/js/6.448beb01.chunk.js"
+  },
+  {
+    "revision": "1342df4fcab80e110a2e",
+    "url": "/static/js/7.fc50da64.chunk.js"
+  },
+  {
+    "revision": "0feb82e2b10614fd37f3",
+    "url": "/static/js/8.b99afa81.chunk.js"
+  },
+  {
+    "revision": "24d81b516d80111970d47a7f2061458f",
+    "url": "/static/js/8.b99afa81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "496a88d4575b4985e509",
+    "url": "/static/js/9.e63994db.chunk.js"
+  },
+  {
+    "revision": "587378361b9b4b061e0e",
+    "url": "/static/js/main.3088364d.chunk.js"
+  },
+  {
+    "revision": "f006780fb3f8ddd7a071",
+    "url": "/static/js/runtime-main.5101a6ed.js"
+  },
+  {
+    "revision": "d0d240133b06aa1932c38bb233c462a6",
+    "url": "/static/media/unicoon.d0d24013.png"
+  }
+]);
